b7fbf5f1dabcf511b984e02348eb51ec5224592a
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-28 17:59:34 -0400

    Make player Node2D

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,9 @@
-extends Node
+extends Node2D
 
 var hp: int = 5
 var hand: Array[Card] = []
 
 func pick(card: Card) -> void:
+	card.position.y = position.y 
+	card.position.x = position.x + len(hand) * 75
 	hand.append(card)
