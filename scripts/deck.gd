d7293f7e9d85075a43ec9bcb840ed8282f7d6cc6
RyanMahajan <ryanmahajan2005@gmail.com>
2025-03-28 01:30:22 -0400

    adding correct deck.gd file

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -1,1 +1,31 @@
 extends Node2D
+
+var card_scene = preload("res://scenes/card.tscn")
+
+var deck: Array = []
+
+@export var is_default: bool = true
+@export var deck_size: int = 52
+
+func _ready():
+	initialize_deck()
+
+func initialize_deck():
+	var card = card_scene.instantiate()
+	if is_default:
+		for i in range(1, card.RANK.values().size()):
+			for j in range(1, card.SUIT.values().size()):
+				card = card_scene.instantiate()
+				card.rank = i
+				card.suit = j
+				card.type = card.NORMAL
+				deck.append(card)
+				add_child(card)
+	else:
+		for i in range(1, deck_size):
+			card = card_scene.instantiate()
+			deck.append(card)
+			add_child(card)
+
+func shuffle_deck():
+	deck.shuffle()
