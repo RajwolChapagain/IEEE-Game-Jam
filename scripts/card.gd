dd22ca0bb4779bc283ef8596f343008123f86cd6
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-28 15:54:15 -0400

    Implement pretty string method | Update 10 pt 2

--- scripts/card.gd
+++ scripts/card.gd
@@ -2,8 +2,12 @@ extends Area2D
 class_name Card
 
 enum RANK {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
-enum SUIT {SPADE, CLUB, DIAMOND, HEART}
-enum TYPE {NORMAL, FIRE, WATER, TREE}
+enum SUIT {HEART, CLUB, DIAMOND, SPADE}
+enum TYPE {NORMAL, TREE, WATER, FIRE}
+
+const RANK_STRINGS = ["Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"]
+const SUIT_STRINGS = ["Hearts", "Clubs", "Diamonds", "Spades"]
+const TYPE_STRINGS = ["Normal", "Tree", "Water", "Fire"]
 
 var rank: int = randi_range(0, RANK.values().size()-1)
 var suit: int = randi_range(0, SUIT.values().size()-1)
@@ -26,15 +30,19 @@ func update_card():
 	Type_Sprite.frame = type
 	
 	if suit == SUIT.SPADE || suit == SUIT.CLUB:
-		Rank_Sprite.frame = rank + 12
-	else:
 		Rank_Sprite.frame = rank
+	else:
+		Rank_Sprite.frame = rank + 13
 	if !face_up:
 		Flip_Sprite.visible = true
 	else:
 		Flip_Sprite.visible = false
 
-
+# Emit signal when clicked
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("pick"):
 		card_clicked.emit(self)
+		
+# Returns a readable string representing the card
+func get_pretty_string() -> String:
+	return str(TYPE_STRINGS[type]) + " " + str(RANK_STRINGS[rank]) + " of " + str(SUIT_STRINGS[suit])
