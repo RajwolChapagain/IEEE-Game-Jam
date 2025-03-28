d12fe54540371e44d5677f9f80e8cacafee55ef2
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-28 16:50:59 -0400

    Return integer instead of bool

--- scripts/calculator.gd
+++ scripts/calculator.gd
@@ -1,6 +1,14 @@
 extends Object
 class_name Calculator
 
-# Returns true if card1 beats card2
-static func compare(card1: Card, card2: Card) -> bool:
-	return card1.rank > card2.rank
+# Returns:
+#  1: if card1 beats card2
+# -1: if card2 beats card1
+#  0: if it's a tie 
+static func compare(card1: Card, card2: Card) -> int:
+	if card1.rank > card2.rank:
+		return 1
+	elif card1.rank < card2.rank:
+		return -1
+	else:
+		return 0
