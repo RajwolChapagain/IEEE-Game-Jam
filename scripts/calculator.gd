0e40820b13e59c7f0af67fb154b6e64f63732857
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-29 03:39:12 -0400

    asdf

--- scripts/calculator.gd
+++ scripts/calculator.gd
@@ -5,10 +5,10 @@ class_name Calculator
 #  1: if card1 beats card2
 # -1: if card2 beats card1
 #  0: if it's a tie 
-static func compare(card1: Card, card2: Card) -> int:
-	if card1.rank > card2.rank:
+static func compare(val1: int, val2: int) -> int:
+	if val1 > val2:
 		return 1
-	elif card1.rank < card2.rank:
+	elif val1 < val2:
 		return -1
 	else:
 		return 0
--- scripts/card.gd
+++ scripts/card.gd
@@ -14,11 +14,13 @@ var suit: int = randi_range(0, SUIT.values().size()-1)
 var type: int = randi_range(0, TYPE.values().size()-1)
 var face_up: bool = true
 var hovered: bool = true
+var applied_effects = []
 
 @export var Rank_Sprite: AnimatedSprite2D
 @export var Suit_Sprite: AnimatedSprite2D
 @export var Type_Sprite: AnimatedSprite2D
 @export var Flip_Sprite: Sprite2D
+@export var Effects_Sprite_Array: Array[AnimatedSprite2D]
 
 signal card_clicked(card)
 
@@ -35,8 +37,10 @@ func update_card():
 		Rank_Sprite.frame = rank + 13
 	if !face_up:
 		Flip_Sprite.visible = true
+		hide_effects()
 	else:
 		Flip_Sprite.visible = false
+		display_effects()
 
 # Emit signal when clicked
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
@@ -46,3 +50,20 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 # Returns a readable string representing the card
 func get_pretty_string() -> String:
 	return str(TYPE_STRINGS[type]) + " " + str(RANK_STRINGS[rank]) + " of " + str(SUIT_STRINGS[suit])
+	
+func get_comparison_val() -> int:
+	var comp_val = rank
+	for effect in applied_effects:
+		comp_val = Relic.apply_relic(comp_val, effect)
+	return comp_val
+
+func display_effects() -> void:
+	for effect in applied_effects:
+		var anim_sprite = Effects_Sprite_Array.pick_random()
+		anim_sprite.visible = true
+		anim_sprite.frame = effect
+
+func hide_effects() -> void:
+	for animated_sprite in Effects_Sprite_Array:
+		animated_sprite.visible = false
+	
