96c496c175d037c642aae722913e8b116f4de03d
Rajwol Chapagain <rajwolchapagain@gmail.com>
2025-03-29 09:17:52 -0400

    Revert "The final push"
    This reverts commit 55afabbb915f1295d3c426ab18d2cade9eb4bf20.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,4 +4,3 @@ func load_new_enemy(resource: EnemyStats):
 	hp = resource.max_hp
 	$HealthBar.max_value = hp
 	$HealthBar.value = hp
-	$Portrait.texture = resource.portrait
--- scripts/enemy_stats.gd
+++ scripts/enemy_stats.gd
@@ -2,4 +2,3 @@ extends Resource
 class_name EnemyStats
 
 @export var max_hp: int
-@export var portrait: Texture2D
